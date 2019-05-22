872e8dfa4155cc328a4decfc6a02715dc16f730f
Alanis <alanis.selene@hotmail.com>
2019-05-21 21:39:40 -0300

    Validacion password

--- login.php
+++ login.php
@@ -1,4 +1,30 @@
-  <?php require 'includes/nav.php'; ?>
+  <?php require 'includes/nav.php';
+
+  $errors = [];
+
+if(!empty($_POST)){
+
+     if(empty($_POST['password'])){
+       $errors['password'][] = 'El campo contraseña está vacío';
+     }
+     if(empty($_POST['passwordConfirm'])){
+         $errors['paswword'][] = 'El campo de confirmación de contraseña está vacio';
+     }
+     if(($_POST['password']) < 8){
+        $errors['password'][] = 'La contraseña debe tener al menos 8 caracteres';
+    }
+    if($_POST['password'] != $_POST['passwordConfirm']){
+       $errors['passwordConfirm'][] = 'Las contraseñas no coinciden' ;
+    }
+
+}
+
+
+
+
+
+
+  ?>
 
 <!DOCTYPE html>
 <html>
@@ -56,21 +82,37 @@
               <p class="form-row form-row-wide"> <label for="reg_nombre">Nombre&nbsp;<span class="required">*</span></label> <input type="text"
                   class="input-text" name="nombre" id="reg_nombre" autocomplete="nombre"></p>
 
+
               <!-- FOMULARIO APELLIDO -->
               <p class="form-row form-row-wide"> <label for="reg_apellido">Apellido&nbsp;<span class="required">*</span></label> <input type="text"
                   class="input-text" name="apellido" id="reg_apellido" autocomplete="apellido"></p>
 
                 <!-- FOMULARIO PAIS DE NACIMIENTO -->
-              <p class="form-row form-row-wide"> <label for="reg_pais">País de Nacimiento&nbsp;<span class="required">*</span></label> <input type="text"
-                  class="input-text" name="pais" id="reg_pais" autocomplete="pais"></p>
+              <p class="form-row form-row-wide">
+              <label for="reg_pais">País de nacimiento&nbsp;<span class="required">*</span></label>
+              <select class="form-control"  name="pais" id="reg_pais">
+              <option value="ar">Argentina</option>
+              <option value="pry">Paraguay</option>
+              <option value="ur">Uruguay</option>
+              <option value="br">Brasil</option>
+              <option value="pr">Perú</option>
+              </select>
+            </p>
+
 
               <!-- FOMULARIO AVATAR -->
               <p class="form-row form-row-wide"> <label for="reg_avatar">Avatar&nbsp;<span class="required">*</span></label> <input type="text"
                   class="input-text" name="avatar" id="reg_avatar" autocomplete="avatar"></p>
 
                 <!-- FOMULARIO SEXO -->
-              <p class="form-row form-row-wide"> <label for="reg_sexo">Sexo&nbsp;<span class="required">*</span></label> <input type="text"
-                  class="input-text" name="sexo" id="reg_sexo" autocomplete="sexo"></p>
+              <p class="form-row form-row-wide"> <label for="reg_sexo">Sexo&nbsp;<span class="required">*</span></label> <div class="custom-control custom-radio">
+              <input type="radio" id="masculino" name="sexo" class="custom-control-input">
+              <label class="custom-control-label" for="masculino">Masculino</label>
+             </div>
+              <div class="custom-control custom-radio">
+                <input type="radio" id="femenino" name="sexo" class="custom-control-input">
+                <label class="custom-control-label" for="femenino">Femenino</label>
+              </div></p>
 
               <!-- FOMULARIO ACEPTO TERMINOS -->
               <label class="">
@@ -80,6 +122,7 @@
               <!-- FOMULARIO CONTRASEÑA -->
               <p class="form-row form-row-wide"> <label for="reg_password">Contraseña&nbsp;<span class="required">*</span></label> <input type="password"
                   class="input-text" name="password" id="reg_password" autocomplete="new-password"></p>
+                  <p><?=$errors['password'][0] ?? ''?></p>
 
             <!-- FOMULARIO CONFIRMAR CONTRASEÑA -->
             <p class="form-row form-row-wide"> <label for="reg_passwordConfirm">Confirmar Contraseña&nbsp;<span class="required">*</span></label> <input type="password"
